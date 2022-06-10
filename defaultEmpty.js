c3cc51d3d0c43508f8c511391568e6875f33169f
bjornoleh <bjorn.haugsgjerd@gmail.com>
2022-06-10 23:48:26 +0200

    Default empty mw after TDD added to oref0

---
+++ defaultEmpty.js
@@ -0,0 +1,5 @@
+function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory, preferences, basalprofile, tdd, tdd_averages) {
+    // modify anything
+    // return any reason what has changed.
+    return "Nothing changed";
+}
